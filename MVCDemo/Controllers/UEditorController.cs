b67e2d6223f140b97afd3a146b949ec3f5970bac
zhangsan <hebaidong1990@126.com>
2017-03-23 15:29:47 +0800

    ueditor

---
+++ MVCDemo/Controllers/UEditorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class UEditorController : Controller
+    {
+        // GET: UEditor
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetAll()
+        {
+            return View();
+        }
+    }
+}
